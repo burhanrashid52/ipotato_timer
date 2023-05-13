586dd67d9d4c860672f8d159ee5d4569a61c0710
Burhanuddin Rashid <burhanuddin@shiny.team>
2023-05-13 09:36:06 +0530

    TaskDataBuilder in task test

--- test/data/task_test.dart
+++ test/data/task_test.dart
@@ -1,19 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ipotato_timer/data/data_source.dart';
 
 import '../test_helper.dart';
 
 void main() {
+  final taskBuilder = TaskDataBuilder(
+    id: 0,
+    title: 'T1',
+    description: 'D1',
+    duration: Duration.zero,
+  );
   group('Tasks', () {
     test(
       'Return isFinished true when elapsed time is more than given duration',
       () {
-        final task = Task(
-          id: 0,
-          title: '',
-          duration: const Duration(seconds: 5),
-          elapsedDuration: const Duration(seconds: 6),
-        );
+        final task = taskBuilder
+            .duration(const Duration(seconds: 5))
+            .elapsedDuration(const Duration(seconds: 6))
+            .build();
         expect(task.isFinished, true);
       },
     );
@@ -21,12 +24,10 @@ void main() {
     test(
       'Return isFinished false when elapsed time is less than given duration',
       () {
-        final task = Task(
-          id: 0,
-          title: '',
-          duration: const Duration(seconds: 5),
-          elapsedDuration: const Duration(seconds: 4),
-        );
+        final task = taskBuilder
+            .duration(const Duration(seconds: 5))
+            .elapsedDuration(const Duration(seconds: 4))
+            .build();
         expect(task.isFinished, false);
       },
     );
@@ -34,12 +35,10 @@ void main() {
     test(
       'Return isFinished true when elapsed time is equal to duration',
       () {
-        final task = Task(
-          id: 0,
-          title: '',
-          duration: const Duration(seconds: 5),
-          elapsedDuration: const Duration(seconds: 5),
-        );
+        final task = taskBuilder
+            .duration(const Duration(seconds: 5))
+            .elapsedDuration(const Duration(seconds: 5))
+            .build();
         expect(task.isFinished, true);
       },
     );
@@ -47,36 +46,28 @@ void main() {
     test(
       'Return isRunning true when we have started time and not finished',
       () {
-        final task = Task(
-          id: 0,
-          title: '',
-          duration: const Duration(seconds: 5),
-          startedAt: DateTime.now(),
-        );
+        final task = taskBuilder
+            .duration(const Duration(seconds: 5))
+            .startedAt(DateTime.now())
+            .build();
         expect(task.isRunning, true);
       },
     );
     test(
       'Return isRunning false when we do not have started time',
       () {
-        final task = Task(
-          id: 0,
-          title: '',
-          duration: const Duration(seconds: 5),
-        );
+        final task = taskBuilder.duration(const Duration(seconds: 5)).build();
         expect(task.isRunning, false);
       },
     );
     test(
       'Return isRunning false when we have started time but finished',
       () {
-        final task = Task(
-          id: 0,
-          title: '',
-          duration: const Duration(seconds: 5),
-          elapsedDuration: const Duration(seconds: 5),
-          startedAt: DateTime.now(),
-        );
+        final task = taskBuilder
+            .duration(const Duration(seconds: 5))
+            .startedAt(DateTime.now())
+            .elapsedDuration(const Duration(seconds: 5))
+            .build();
         expect(task.isRunning, false);
       },
     );
@@ -84,23 +75,20 @@ void main() {
 
   group('Total Elapsed duration', () {
     test('Return elapsed duration when no start time found', () {
-      final task = Task(
-        id: 0,
-        title: '',
-        duration: const Duration(seconds: 5),
-        elapsedDuration: const Duration(seconds: 2),
-      );
+      final task = taskBuilder
+          .duration(const Duration(seconds: 5))
+          .elapsedDuration(const Duration(seconds: 2))
+          .build();
+
       expect(task.totalElapsed, const Duration(seconds: 2));
     });
     test('Return total elapsed duration when start time found', () {
       runFakeClock(DateTime(2022, 5, 13, 8, 55), () {
-        final task = Task(
-          id: 0,
-          title: '',
-          duration: const Duration(minutes: 10),
-          elapsedDuration: const Duration(minutes: 2),
-          startedAt: DateTime(2022, 5, 13, 8, 51),
-        );
+        final task = taskBuilder
+            .duration(const Duration(minutes: 10))
+            .elapsedDuration(const Duration(minutes: 2))
+            .startedAt(DateTime(2022, 5, 13, 8, 51))
+            .build();
         expect(task.totalElapsed, const Duration(minutes: 6));
       });
     });
